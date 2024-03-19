d5e9f5fe9664a00fce682f02593041e408f53e44
Alex <Alehandru753202@mail.ru>
2024-03-20 00:36:27 +0200

    Comit3

--- 02. Iterating over arrays/Check if an array contains two numbers/src/Main.java
+++ 02. Iterating over arrays/Check if an array contains two numbers/src/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
-        System.out.println("Alex");
+        Scanner scanner = new Scanner(System.in);
+
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int sum = n + m;
+        int sumArray = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            sumArray += array[i];
+            if (sumArray != sum) {
+                sumArray = array[i];
+            } else {
+                break;
+            }
+        }
+        System.out.println(sumArray != sum ? false : true);
     }
 }
